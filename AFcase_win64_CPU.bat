1585f5ad7635d3a1df49c4c77b1ac89e96f826aa
AtefeFz <nikki.fazlollahi@gmail.com>
2020-07-06 13:24:56 -0400

    issue_1
    Error in loading XML file with executing windows batch file

---
+++ AFcase_win64_CPU.bat
@@ -0,0 +1,58 @@
+
+setlocal EnableDelayedExpansion
+rem Don't remove the two jump line after than the next line [set NL=^]
+set NL=^
+
+
+rem "name" and "dirout" are named according to the testcase
+
+set name=AFcase
+set dirout=%name%_out
+set diroutdata=%dirout%\data
+
+rem "executables" are renamed and called from their directory
+
+set dirbin=../../../bin/windows
+set gencase="%dirbin%/GenCase4_win64.exe"
+set dualsphysicscpu="%dirbin%/DualSPHysics4.4CPU_win64.exe"
+set dualsphysicsgpu="%dirbin%/DualSPHysics4.4_win64.exe"
+set boundaryvtk="%dirbin%/BoundaryVTK4_win64.exe"
+set partvtk="%dirbin%/PartVTK4_win64.exe"
+set partvtkout="%dirbin%/PartVTKOut4_win64.exe"
+set measuretool="%dirbin%/MeasureTool4_win64.exe"
+set computeforces="%dirbin%/ComputeForces4_win64.exe"
+set isosurface="%dirbin%/IsoSurface4_win64.exe"
+set flowtool="%dirbin%/FlowTool4_win64.exe"
+set floatinginfo="%dirbin%/FloatingInfo4_win64.exe"
+
+:menu
+if exist %dirout% ( 
+	set /p option="The folder "%dirout%" already exists. Choose an option.!NL!  [1]- Delete it and continue.!NL!  [2]- Execute post-processing.!NL!  [3]- Abort and exit.!NL!"
+	if "!option!" == "1" goto run else (
+		if "!option!" == "2" goto postprocessing else (
+			if "!option!" == "3" goto fail else ( 
+				goto menu
+			)
+		)
+	)
+)
+
+:run
+rem "dirout" to store results is removed if it already exists
+if exist %dirout% rd /s /q %dirout%
+
+rem CODES are executed according the selected parameters of execution in this testcase
+rem Executes GenCase4 to create initial files for simulation.
+%gencase% %name%_Def %dirout%/%name% -save:all
+if not "%ERRORLEVEL%" == "0" goto fail
+
+:success
+echo All done
+goto end
+
+:fail
+echo Execution aborted.
+
+:end
+pause
+
---
+++ read_me.txt
@@ -0,0 +1,4 @@
+Hello all,
+
+I am quite new with DualPhysics. I have read the XML guide and created my own case XML file. I tried to run that just to make sure the geometry is correct using the batch script as follows and I am getting an error 
+
